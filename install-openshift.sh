38a4d918f90de2d7b41175c5762887a5f0b4fdee
Arun Kumar <arunbsar@gmail.com>
2019-07-03 21:52:53 +0100

    Added solution from a bugreport

--- install-openshift.sh
+++ install-openshift.sh
@@ -116,7 +116,7 @@ yum -y --enablerepo=epel install ansible.rpm
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) ${DOMAIN}  
+${IP}		${DOMAIN} $(hostname)
 EOD
 
 if [ -z $DISK ]; then 
